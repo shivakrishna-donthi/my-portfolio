a2e04d26f3f22cfd5c97aa16ce634a2a062ec4c2
Shiva Krishna Donthi <75441392+shivakrishna-donthi@users.noreply.github.com>
2021-02-25 15:43:15 +0530

    Initial angular project setup
    Angular bootstrap boilerplate

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
