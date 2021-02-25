1edfd1cbfa9e13ac27e3700817b86c987ea2e3c8
Shiva Krishna Donthi <75441392+shivakrishna-donthi@users.noreply.github.com>
2021-02-25 21:03:00 +0530

    bootstrap template with content and font-awesome icons

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faLinkedinIn, faGithub, faStackOverflow, faFacebookF, faHtml5, faCss3Alt, faJsSquare, faAngular, faReact, faNodeJs, faSass, faLess, faGulp, faGrunt, faNpm, faJenkins, faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faTrophy, faCheckCircle, faLocationArrow, faPhoneAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  faLinkedinIn = faLinkedinIn;
+  faGithub = faGithub;
+  faStackOverflow = faStackOverflow;
+  faFacebook = faFacebookF;
+  faHtml5 = faHtml5;
+  faCss3Alt = faCss3Alt;
+  faJsSquare = faJsSquare;
+  faAngular = faAngular;
+  faReact = faReact;
+  faNodeJs = faNodeJs;
+  faSass = faSass;
+  faLess = faLess;
+  faGulp = faGulp;
+  faGrunt = faGrunt;
+  faNpm = faNpm;
+  faJenkins = faJenkins;
+  faBootstrap = faBootstrap;
+  faTrophy = faTrophy;
+  faCheckCircle = faCheckCircle;
+  faLocationArrow = faLocationArrow;
+  faPhoneAlt = faPhoneAlt;
+  faEnvelope = faEnvelope;
 
   constructor() { }
 
